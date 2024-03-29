877de0ee75e68a43701e4945e6d74deb60c43e63
SaoriKabuchi <rkabuchi@gmail.com>
2024-03-28 22:22:44 -0300

    projeto finalizado

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {
+  EMPTY,
+  catchError,
+  debounceTime,
+  filter,
+  map,
+  of,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
+import { Item, LivrosResultado } from './../../models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css'],
+})
+export class ListaLivrosComponent {
+  campoBusca = new FormControl();
+  mensagemErro = '';
+  livrosResultado: LivrosResultado;
+
+  constructor(private service: LivroService) {}
+
+  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('fluxo')),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map((resultado) => (this.livrosResultado = resultado)),
+    catchError((erro) => {
+      console.log(erro);
+      return of();
+    })
+  );
+
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('fluxo')),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    tap((retornoAPI) => console.log(retornoAPI)),
+    map((resultado) => resultado.items ?? []),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError((erro) => {
+      console.log(erro);
+      return throwError(
+        () =>
+          new Error(
+            (this.mensagemErro =
+              'Ops, ocorreu um erro. Recarregue a aplicação!')
+          )
+      );
+      // this.mensagemErro = 'Ocorreu um erro, recarregue a app';
+      // return EMPTY;
+    })
+  );
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map((item) => {
+      return new LivroVolumeInfo(item);
+    });
+  }
+}
